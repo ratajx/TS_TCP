eca9d644531b256654c64602c55a9c163c01fbfc
ratajx <ratajx93@gmail.com>
2014-06-29 16:18:56 +0200

    serwer

---
+++ serwer/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace TS_42_serwer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Wymagana metoda wsparcia projektanta - nie należy modyfikować
+        /// zawartość tej metody z edytorem kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.połączenie = new System.ComponentModel.BackgroundWorker();
+            this.odbieranie = new System.ComponentModel.BackgroundWorker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.zmiana_poł = new System.ComponentModel.BackgroundWorker();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(46, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Włącz serwer";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(6, 85);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(170, 60);
+            this.richTextBox1.TabIndex = 16;
+            this.richTextBox1.Text = "";
+            // 
+            // połączenie
+            // 
+            this.połączenie.WorkerSupportsCancellation = true;
+            this.połączenie.DoWork += new System.ComponentModel.DoWorkEventHandler(this.połączenie_DoWork);
+            // 
+            // odbieranie
+            // 
+            this.odbieranie.WorkerSupportsCancellation = true;
+            this.odbieranie.DoWork += new System.ComponentModel.DoWorkEventHandler(this.odbieranie_DoWork);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(56, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(80, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(6, 19);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(170, 60);
+            this.richTextBox2.TabIndex = 17;
+            this.richTextBox2.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(182, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(60, 60);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "Wyślij";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(6, 21);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.ReadOnly = true;
+            this.richTextBox3.Size = new System.Drawing.Size(218, 235);
+            this.richTextBox3.TabIndex = 19;
+            this.richTextBox3.Text = "";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 7);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(194, 90);
+            this.groupBox1.TabIndex = 22;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Port";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.richTextBox2);
+            this.groupBox2.Controls.Add(this.richTextBox1);
+            this.groupBox2.Location = new System.Drawing.Point(6, 117);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(252, 155);
+            this.groupBox2.TabIndex = 23;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Wiadomości";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.richTextBox3);
+            this.groupBox3.Location = new System.Drawing.Point(264, 5);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(230, 270);
+            this.groupBox3.TabIndex = 24;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Log serwera";
+            // 
+            // zmiana_poł
+            // 
+            this.zmiana_poł.WorkerSupportsCancellation = true;
+            this.zmiana_poł.DoWork += new System.ComponentModel.DoWorkEventHandler(this.zmiana_poł_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(505, 279);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.ComponentModel.BackgroundWorker połączenie;
+        private System.ComponentModel.BackgroundWorker odbieranie;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.ComponentModel.BackgroundWorker zmiana_poł;
+    }
+}
+
---
+++ serwer/Form1.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+using komunikaty;
+
+namespace TS_42_serwer
+{
+    public partial class Form1 : Form
+    {
+        private TcpListener listener = null;
+        private TcpClient klient = null;
+        private BinaryReader r = null;  
+        private BinaryWriter w=null;
+        private bool polaczenie=false;
+        private bool zmiana_portu = false;
+        private bool z = true;
+        private int port = 0;
+        private int nowy_port = 12345;
+        
+        
+       
+        public Form1()
+        {
+            InitializeComponent();
+            button2.Enabled = false;
+        }
+
+
+        private void wlacz(int x)
+        {
+
+            listener = new TcpListener(IPAddress.Any, x);
+            listener.Start();
+
+            this.Invoke(new Action(() =>
+            {
+                button1.Enabled = false;
+                richTextBox3.AppendText(komunikaty.KomunikatySerwera.On + " port:" + port + "\n");
+                richTextBox3.ScrollToCaret();
+            }));
+            try
+            {
+                klient = listener.AcceptTcpClient();
+            }
+            catch (SocketException a)
+            {
+
+            }
+            if (klient.Connected)
+            {
+                this.Invoke(new Action(() =>
+                {
+                    button2.Enabled = true;
+                    richTextBox3.ScrollToCaret();
+                }));
+                polaczenie = true;
+
+            }
+
+            NetworkStream stream = klient.GetStream();
+            r = new BinaryReader(stream);
+            w = new BinaryWriter(stream);
+            this.Invoke(new Action(() =>
+               {
+                   richTextBox3.AppendText(komunikaty.KomunikatySerwera.OK + " z klientem port: " + x + "\n");
+                   Thread.Sleep(1000);
+                   napisz(KomunikatySerwera.OK);
+                   Thread.Sleep(1000);
+
+               }));
+
+
+            if ((klient.Connected) && (port != nowy_port))
+                odbieranie.RunWorkerAsync();
+            else
+                odbierz();
+            if (z)
+                zmiana();
+
+        }
+        
+        private void zmiana()
+        {
+            zmiana_portu = true;
+            z = false;
+            port = nowy_port;
+            MessageBox.Show("Nastąpi zmiana portu na port: 12345","Komunikat serwera");
+            napisz(KomunikatySerwera.ZmianaPortu);
+            napisz(nowy_port.ToString());
+            Thread.Sleep(500);
+
+            this.Invoke(new Action(()=> richTextBox3.AppendText("Wysłano żadanie #zmiany portu#\n")));
+            rozlacz();
+            Thread.Sleep(500);
+
+            wlacz(port);
+            
+        }
+
+        public void napisz(String a)
+            {
+                try
+                {
+                    w.Write(a);
+                }
+                catch
+                {
+                    this.Invoke(new Action(() =>
+                    {
+                        button1.Enabled = false;
+                        button2.Enabled = true;
+                    }));
+                    
+                }
+            }
+
+        private void wyswietl(RichTextBox o, string tekst)
+        {
+            this.Invoke(new Action(() =>
+               {
+                   o.Focus();
+                   o.AppendText(tekst);
+                   o.ScrollToCaret();
+               }));
+        }
+
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            połączenie.RunWorkerAsync();
+        }
+
+
+        private void połączenie_DoWork(object sender, DoWorkEventArgs e)
+        {
+            bool port_zmiana = false;
+            if (!zmiana_portu)
+            {
+                bool petla = true;
+                
+                if (petla)
+                {
+                    if (textBox1.Text != "")
+                    {
+                        bool jest_liczba = true;
+                        try
+                        {
+                            Convert.ToInt16(textBox1.Text);
+                        }
+                        catch
+                        {
+
+                            MessageBox.Show("Numer portu zawiera niepoprawne znaki, wprowadź liczbę");
+                            jest_liczba = false;
+                        }
+                        if (jest_liczba)
+                        {
+                            port = Convert.ToInt32(textBox1.Text);
+                            wlacz(port);
+                            petla = false;
+                        }
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Podaj port");
+                    }
+                }
+            }
+            if(port_zmiana)
+            wlacz(port);
+            port_zmiana = true;
+        }
+
+        private void rozlacz()
+        {
+            
+            this.Invoke(new Action(() =>
+            {
+                button1.Enabled = true;
+                button2.Enabled = false;
+                richTextBox3.AppendText(KomunikatySerwera.Rozlacz + "\n");
+             }));
+            listener.Stop();
+            listener = null;
+            klient.Close();
+            klient = null;
+            połączenie.CancelAsync();
+            odbieranie.CancelAsync();
+              
+        }
+
+        private void komunikat(String tekst)
+        {
+            switch (tekst)
+            {
+                case "#Rozlaczono#":
+                    {
+                        this.Invoke(new Action(() => richTextBox3.AppendText("Klient rozłączył się\n")));
+                        listener.Stop();
+                        klient.Close();
+                        połączenie.CancelAsync();
+                        odbieranie.CancelAsync();
+                        this.Invoke(new Action(() =>
+                        {
+                            button2.Enabled = false;
+                            button1.Enabled = true;
+                        }));
+                        break;
+                    }
+            }
+
+        }
+
+        private void odbierz()
+        {
+
+            string tekst;
+            if (polaczenie)
+            {
+                try
+                {
+                    while ((tekst = r.ReadString()) != "")
+                    {
+                        if (tekst[0] != '#')
+                        {
+                            wyswietl(this.richTextBox1, tekst + '\n');
+                        }
+                        else
+                            komunikat(tekst);
+                    }
+                }
+                catch
+                {
+                    this.Invoke(new Action(() =>
+                    {
+                        button1.Enabled = true;
+                        button2.Enabled = false;
+                    }));
+                    
+
+                }
+            }
+        
+        }
+        private void odbieranie_DoWork(object sender, DoWorkEventArgs e)
+        {
+            odbierz();
+        }
+        
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (polaczenie)
+            {
+                
+                listener.Stop();
+                if (klient != null) klient.Close();
+                polaczenie = false;
+            }
+           połączenie.CancelAsync();
+           odbieranie.CancelAsync();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            napisz(richTextBox2.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void zmiana_poł_DoWork(object sender, DoWorkEventArgs e)
+        {
+            wlacz(port);
+        }
+        }
+}
